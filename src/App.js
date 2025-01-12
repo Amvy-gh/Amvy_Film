ec79ec0b3beb8b0aaaa0186b21ac34162e9e97f7
Amvy-gh <anjesmanik@gmail.com>
2025-01-12 21:51:41 +0700

    Make an Intro

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 import './App.css';
 import MyNavbar from './components/Navbar.js';
+import Intro from './components/Intro.js';
 import "./style/landingpage.css";
 
 function App() {
   return (
     <div>
-      <div className="myBG">
-        <MyNavbar />
-      </div>
+      {/* {Intro Section} */}
+        <div className="myBG">
+          <MyNavbar />
+          <Intro />
+        </div>  
+      {/* {End of Intro} */}
     </div>
   );
 }
